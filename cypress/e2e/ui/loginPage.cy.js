c8a5d43d28d6e6ae044e88c32e2621f8dfb8b46f
Michels <michelsegura10@gmail.com>
2025-09-15 09:38:39 -0600

    Test de login

--- cypress/e2e/ui/loginPage.cy.js
+++ cypress/e2e/ui/loginPage.cy.js
@@ -1,9 +1,13 @@
 import loginPage from '../../pages/loginPage'
 
-describe('Login',function(){
-    it('Login exitoso',function(){
-        //Validacion login
+describe('Login', function(){
+
+    beforeEach(()=>{
         loginPage.visitPage()
+    })
+
+    it('Login exitoso', function(){
+        //Validacion login
         loginPage.typeUsername('standard_user')
         loginPage.typePassword('secret_sauce')
         loginPage.loginBtn()
@@ -12,4 +16,17 @@ describe('Login',function(){
         let url = 'https://www.saucedemo.com/inventory.html'
         cy.url(url).should('eq','https://www.saucedemo.com/inventory.html')
     })
+
+    it('Login con campos vacios', function(){
+        loginPage.loginBtn()
+        cy.get('h3').should('contain','Epic sadface: Username is required')
+    })
+
+    it('Login con campos erroneos', function(){
+        loginPage.typeUsername('standard_user')
+        loginPage.typePassword('123')
+        loginPage.loginBtn()
+        cy.get('h3').should('contain','Epic sadface: Username and password do not match any user in this service')
+    })
+
 })
