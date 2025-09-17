5700da83308e094659821401657c3302e513c388
Michels <michelsegura10@gmail.com>
2025-09-17 11:55:41 -0600

    Nuevo fixture y test para createBooking + assertions

--- cypress/e2e/api/booking.cy.js
+++ cypress/e2e/api/booking.cy.js
@@ -3,15 +3,27 @@ import bookingService from "../../services/bookingService"
 describe('Restful-booker E2E', function(){
 
     beforeEach(()=>{
-        cy.fixture('booking').as('booking')
+        cy.fixture('credentials').as('credentials')
+        cy.fixture('bookingData').as('bookingData')
     })
 
     it('loginAPI', function(){
-        const user = this.booking.usuarioAPI
+        const user = this.credentials.usuarioAPI
 
         bookingService.loginAPI(user.username,user.password).then((resp)=>{
             expect(resp.status).to.eq(200)
             Cypress.env('token',resp.body.token)
+        })
+    })
+    
+    it('createBooking', function(){
+        const booking = this.bookingData.bookingData
+
+        bookingService.createBooking(booking).then((resp)=>{
+            expect(resp.status).to.eq(200)
+            cy.log(resp.body.bookingid)
+            expect(resp.body.bookingid).to.eq(resp.body.bookingid)
+            expect(resp.body.booking.firstname).to.exist
         })
     })
 })
--- cypress/services/bookingService.js
+++ cypress/services/bookingService.js
@@ -16,6 +16,19 @@ class BookingService{
             }
         })
     }
+
+    createBooking(booking){
+        return cy.request({
+            method: 'POST',
+            url:'https://restful-booker.herokuapp.com/booking',
+            headers:{
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: booking
+        })
+    }
+
 }
 
 export default new BookingService()
